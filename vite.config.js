ed548a19b84a50d8f719a0abb687053bf016ec55
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-06-06 04:30:42 +0900

    Minor bug fixes

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 /* eslint-disable no-undef */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-pokedex/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'slides.html'),
-      },
-    },
-  },
+  base: "/react-pokedex/"
 });
