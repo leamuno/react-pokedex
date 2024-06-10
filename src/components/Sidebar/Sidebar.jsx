1455d129ff5b907f089086596bdb2a1049675505
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-06-10 23:12:48 +0900

    Updates post method to new api!

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,18 +2,48 @@ import React from 'react';
 import "./Sidebar.css";
 import logo from '../../assets/logo_w_context2.png';
 
-function Sidebar({setPokemons}) {
+function Sidebar({ setPokemons }) {
   function addPokemon(form) {
-    const url = "https://poke-api-97bb8825da70.herokuapp.com/api/v1/pokemons";
+    const formData = new FormData(form);
+    const pokemon = {};
+    formData.forEach((value, key) => {
+      const keys = key.split('[').map(k => k.replace(']', ''));
+      let current = pokemon;
+      for (let i = 0; i < keys.length; i++) {
+        if (i === keys.length - 1) {
+          current[keys[i]] = value;
+        } else {
+          current[keys[i]] = current[keys[i]] || {};
+          current = current[keys[i]];
+        }
+      }
+    });
+
+    // Extract only the value of the "pokemon" key
+    const pokemonData = pokemon.pokemon;
+
+    // Log the JSON data to the console
+    console.log(JSON.stringify(pokemonData));
+
+    const url = "https://nf07ey28qa.execute-api.ap-northeast-1.amazonaws.com/prod/pokemon";
     fetch(url, {
       method: "POST",
-      body: new FormData(form)
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(pokemonData)
     })
       .then(response => response.json())
       .then((newPokemon) => {
-        setPokemons((pokemonList) => [...pokemonList, newPokemon]);
+        // Ensure newPokemon is properly structured and set the state
+        if (newPokemon && newPokemon.Pokemon) {
+          setPokemons((pokemonList) => [...pokemonList, newPokemon.Pokemon]);
+        } else {
+          console.error('Unexpected response structure:', newPokemon);
+        }
         form.reset();
-      });
+      })
+      .catch(error => console.error('Error:', error));
   }
 
   function handleSubmit(event) {
@@ -21,7 +51,7 @@ function Sidebar({setPokemons}) {
     addPokemon(event.currentTarget);
   }
 
-  const types = ["poison","electric","ground","fairy","fire","normal","flying","grass","water","bug","fighting","psychic","rock","steel","ice","ghost","dragon"];
+  const types = ["", "poison", "electric", "ground", "fairy", "fire", "normal", "flying", "grass", "water", "bug", "fighting", "psychic", "rock", "steel", "ice", "ghost", "dragon"];
 
   return (
     <div className="sidebar">
@@ -29,30 +59,50 @@ function Sidebar({setPokemons}) {
         <h3>Add a missing pokemon</h3>
         <form onSubmit={handleSubmit}>
           <div className="input-group mb-3">
-            <span className="input-group-text" id="pokemon-name"><i className="fa-solid fa-mug-saucer form-icons"></i></span>
-            <input name="pokemon[name]" placeholder="pikachu" type="text" className="form-control" aria-describedby="pokemon-name" />
-          </div>
-          <div className="input-group mb-3">
             <span className="input-group-text" id="pokemonID"><i className="fa-solid fa-mug-saucer form-icons"></i></span>
-            <input name="pokemon[pokemonID]" placeholder="25" type="text" className="form-control" aria-describedby="pokemonID" />
+            <input name="pokemon[pokemonID]" placeholder="152" type="text" className="form-control" aria-describedby="pokemonID" />
           </div>
           <div className="input-group mb-3">
-            <span className="input-group-text" id="pokemon-encounter"><i className="fa-solid fa-location-dot form-icons"></i></span>
-            <input name="pokemon[encounter]" placeholder="veridian forest" aria-describedby="pokemon-encounter" type="text" className="form-control" />
-          </div>
-          <div className="mb-3">
-          { types.map((criterion) => {
-              return (
-                <React.Fragment key={criterion}>
-                  <input name="pokemon[types][]" type="checkbox" className="btn-check" id={criterion} autoComplete="off" value={criterion}/>
-                  <label className="btn btn-outline-danger btn-sm mx-1 mb-1" htmlFor={criterion}>{criterion}</label>
-                </React.Fragment>
-              );
-            }) }
+            <span className="input-group-text" id="name"><i className="fa-solid fa-mug-saucer form-icons"></i></span>
+            <input name="pokemon[name]" placeholder="chikorita" type="text" className="form-control" aria-describedby="name" />
           </div>
           <div className="input-group mb-3">
-            <span className="input-group-text" id="pokemon-pictures"><i className="fa-solid fa-camera-retro form-icons"></i></span>
-            <input name="pokemon[pictures]" type="text" className="form-control" aria-describedby="pokemon-picture" placeholder='http://example.com/image.jpg'/>
+            <span className="input-group-text" id="dexId"><i className="fa-solid fa-mug-saucer form-icons"></i></span>
+            <input name="pokemon[dexId]" placeholder="152" type="text" className="form-control" aria-describedby="dexId" />
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="location"><i className="fa-solid fa-location-dot form-icons"></i></span>
+            <input name="pokemon[location]" placeholder="new bark town area" aria-describedby="location" type="text" className="form-control" />
+          </div>
+          <div className="input-group mb-3">
+            <select className="input-group-text" id="type1" name="pokemon[type1]" aria-label="type1">
+              {types.map((criterion) => {
+                return (
+                  <React.Fragment key={criterion}>
+                    <option value={criterion}>{criterion}</option>
+                  </React.Fragment>
+                );
+              })}
+            </select>
+          </div>
+          <div className="input-group mb-3">
+            <select className="input-group-text" id="type2" name="pokemon[type2]" aria-label="type2">
+              {types.map((criterion) => {
+                return (
+                  <React.Fragment key={criterion}>
+                    <option value={criterion}>{criterion}</option>
+                  </React.Fragment>
+                );
+              })}
+            </select>
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="defaultSprite"><i className="fa-solid fa-camera-retro form-icons"></i></span>
+            <input name="pokemon[defaultSprite]" type="text" className="form-control" aria-describedby="defaultSprite" placeholder='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png' />
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="shinySprite"><i className="fa-solid fa-camera-retro form-icons"></i></span>
+            <input name="pokemon[shinySprite]" type="text" className="form-control" aria-describedby="shinySprite" placeholder='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/152.png' />
           </div>
           <div className="d-grid">
             <button type="submit" className="btn btn-danger">Send to Oak</button>
